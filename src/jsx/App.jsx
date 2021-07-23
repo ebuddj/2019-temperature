7fb9d74311ed0ab0865049e92cb3f9cb80abd9cc
Teemo Tebest <teemo.tebest@gmail.com>
2021-07-23 10:08:00 +0200

    Changed the scale

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -15,14 +15,14 @@ let interval;
 // Define constants.
 const yearStart = 1901,
       yearEnd = 2020,
-      scaleMax = 2,
-      scaleMin = -2,
+      scaleMax = 3,
+      scaleMin = -3,
       intervalTimeout = 300,
       avg_temps = [-0.15,-0.28,-0.37,-0.47,-0.26,-0.22,-0.39,-0.43,-0.48,-0.43,-0.44,-0.36,-0.34,-0.15,-0.14,-0.36,-0.46,-0.3,-0.27,-0.27,-0.19,-0.29,-0.27,-0.27,-0.22,-0.11,-0.22,-0.2,-0.36,-0.16,-0.1,-0.16,-0.29,-0.12,-0.2,-0.15,-0.03,0,-0.02,0.12,0.18,0.06,0.09,0.2,0.09,-0.07,-0.03,-0.11,-0.11,-0.17,-0.07,0.01,0.08,-0.13,-0.14,-0.19,0.05,0.06,0.03,-0.03,0.06,0.03,0.05,-0.2,-0.11,-0.06,-0.02,-0.08,0.05,0.03,-0.08,0.01,0.16,-0.07,-0.01,-0.1,0.18,0.07,0.16,0.26,0.32,0.14,0.31,0.16,0.12,0.18,0.32,0.39,0.27,0.45,0.41,0.22,0.23,0.32,0.45,0.33,0.47,0.61,0.39,0.4,0.54,0.63,0.62,0.54,0.68,0.64,0.66,0.54,0.66,0.72,0.61,0.65,0.68,0.74,0.9,1.01,0.92,0.85,0.98,1.02],
       monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
       countries = {'AFG':'Afghanistan','ALB':'Albania','DZA':'Algeria','AND':'Andorra','AGO':'Angola','ATG':'Antigua and Barbuda','ARG':'Argentina','ARM':'Armenia','AUS':'Australia','AUT':'Austria','AZE':'Azerbaijan','BHS':'Bahamas','BHR':'Bahrain','BGD':'Bangladesh','BRB':'Barbados','BLR':'Belarus','BEL':'Belgium','BLZ':'Belize','BEN':'Benin','BTN':'Bhutan','BOL':'Bolivia','BIH':'Bosnia and Herzegovina','BWA':'Botswana','BRA':'Brazil','BRN':'Brunei Darussalam','BGR':'Bulgaria','BFA':'Burkina Faso','BDI':'Burundi','KHM':'Cambodia','CMR':'Cameroon','CAN':'Canada','CPV':'Cabo Verde','CAF':'Central African Republic','TCD':'Chad','CHL':'Chile','CHN':'China','COL':'Colombia','COM':'Comoros','COD':'Congo-Kinshasa','COG':'Congo-Brazzaville','CRI':'Costa Rica','CIV':'Côte d\'Ivoire','HRV':'Croatia','CUB':'Cuba','CYP':'Cyprus','CZE':'Czechia','DNK':'Denmark','DJI':'Djibouti','DMA':'Dominica','DOM':'Dominican Republic','ECU':'Ecuador','EGY':'Egypt','SLV':'El Salvador','GNQ':'Equatorial Guinea','ERI':'Eritrea','EST':'Estonia','ETH':'Ethiopia','FRO':'Faroe Islands','FSM':'Micronesia','FJI':'Fiji','FIN':'Finland','FRA':'France','GAB':'Gabon','GMB':'Gambia','GEO':'Georgia','DEU':'Germany','GHA':'Ghana','GRC':'Greece','GRL':'Greenland','GRD':'Grenada','GTM':'Guatemala','GIN':'Guinea','GNB':'Guinea-Bissau','GUY':'Guyana','HTI':'Haiti','HND':'Honduras','HUN':'Hungary','ISL':'Iceland','IND':'India','IDN':'Indonesia','IRN':'Iran','IRQ':'Iraq','IRL':'Ireland','ISR':'Israel','ITA':'Italy','JAM':'Jamaica','JPN':'Japan','JOR':'Jordan','KAZ':'Kazakhstan','KEN':'Kenya','KIR':'Kiribati','PRK':'North Korea','KOR':'South Korea','KWT':'Kuwait','KGZ':'Kyrgyzstan','LAO':'Lao','LVA':'Latvia','LBN':'Lebanon','LSO':'Lesotho','LBR':'Liberia','LBY':'Libya','LIE':'Liechtenstein','LTU':'Lithuania','LUX':'Luxembourg','MKD':'Republic of North Macedonia','MDG':'Madagascar','MWI':'Malawi','MYS':'Malaysia','MDV':'Maldives','MLI':'Mali','MLT':'Malta','MHL':'Marshall Islands','MRT':'Mauritania','MUS':'Mauritius','MEX':'Mexico','MDA':'Moldova','MCO':'Monaco','MNG':'Mongolia','MAR':'Morocco','MOZ':'Mozambique','MMR':'Myanmar','NAM':'Namibia','NPL':'Nepal','NLD':'Netherlands','NCL':'New Caledonia','NZL':'New Zealand','NIC':'Nicaragua','NER':'Niger','NGA':'Nigeria','MNP':'Northern Mariana Islands','NOR':'Norway','OMN':'Oman','PAK':'Pakistan','PLW':'Palau','PAN':'Panama','PNG':'Papua New Guinea','PRY':'Paraguay','PER':'Peru','PHL':'Philippines','POL':'Poland','PRT':'Portugal','PRI':'Puerto Rico','QAT':'Qatar','MNE':'Montenegro','SRB':'Serbia','ROU':'Romania','RUS':'Russian Federation','RWA':'Rwanda','WSM':'Samoa','STP':'Sao Tome and Principe','SAU':'Saudi Arabia','SEN':'Senegal','SYC':'Seychelles','SLE':'Sierra Leone','SGP':'Singapore','SVK':'Slovakia','SVN':'Slovenia','SLB':'Solomon Islands','SOM':'Somalia','ZAF':'South Africa','SSD':'South Sudan','ESP':'Spain','LKA':'Sri Lanka','KNA':'Saint Kitts and Nevis','LCA':'Saint Lucia','VCT':'Saint Vincent and the Grenadines','SDN':'Sudan','SUR':'Suriname','SWZ':'Eswatini','SWE':'Sweden','CHE':'Switzerland','SYR':'Syrian Arab Republic','TJK':'Tajikistan','TZA':'Tanzania','THA':'Thailand','TLS':'Timor-Leste','TGO':'Togo','TON':'Tonga','TTO':'Trinidad and Tobago','TUN':'Tunisia','TUR':'Turkey','TKM':'Turkmenistan','TUV':'Tuvalu','UGA':'Uganda','UKR':'Ukraine','ARE':'United Arab Emirates','GBR':'United Kingdom','USA':'United States of America','URY':'Uruguay','UZB':'Uzbekistan','VUT':'Vanuatu','VEN':'Venezuela','VNM':'Viet Nam','YEM':'Yemen','ZMB':'Zambia','ZWE':'Zimbabwe'};
 // Use chroma to make the color scale.
-const f = chroma.scale('RdYlBu').domain([scaleMax,0,scaleMin]);
+const f = chroma.scale('RdYlBu').domain([scaleMax, 0, scaleMin]);
 
 let scales = [], temperature = scaleMax;
 while (temperature > scaleMin) {
@@ -164,7 +164,7 @@ class App extends Component {
       temperature = temperature[0].data.reduce((total, current) => total + current.value, 0) / temperature[0].data.length;
       this.setState((state, props) => ({
         active_country_temp:temperature,
-        current_year_average_temp:null
+        current_year_average_temp:temperature
       }));
     }
     // If current country is empty.
@@ -188,7 +188,7 @@ class App extends Component {
       year:year
     }), this.getCurrentYearAverageTemp);
   }
-  handleCountryClick(data, i, status) {
+  handleCountryClick(data, i, status) {
     // If already clicked we want to expand.
     if (status === 'clicked') {
       this.setState((state, props) => ({
@@ -215,14 +215,14 @@ class App extends Component {
     }
   }
   handleSearchChange(event) {
-    let country = Object.keys(countries).filter((key) => (countries[key] === event.target.value))[0] || event.target.value;
+    let country = Object.keys(countries).filter((key) => (countries[key] === event.target.value))[0] || event.target.value;
     this.setState((state, props) => ({
       search_text:country
     }));
     let temperature = this.state.current_data.filter(obj => {
       return obj.country === country;
     });
-    if (temperature.length === 1) {
+    if (temperature.length === 1) {
       this.setState((state, props) => ({
         active_country_id:Object.keys(countries).findIndex((el) => el === country),
         active_country_name:country,
@@ -256,7 +256,7 @@ class App extends Component {
               this.state.current_data && this.state.current_data.map((data, i) => {
                 let country_container_class;
                 let status = '';
-                if (this.state.expand === true) {
+                if (this.state.expand === true) {
                   if (this.state.active_country_id === i) {
                     country_container_class = style.country_container_expanded + ' ' + style.country_container;
                   }
@@ -269,7 +269,7 @@ class App extends Component {
                     status = 'clicked';
                     country_container_class = style.county_container_active + ' ' + style.country_container;
                   }
-                  else if (this.state.active_country_id !== null) {
+                  else if (this.state.active_country_id !== null) {
                     country_container_class = style.county_container_unactive + ' ' + style.country_container;
                   }
                   else {
